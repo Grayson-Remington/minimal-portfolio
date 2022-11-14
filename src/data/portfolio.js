efe0387d5d139f6ea513d5fefcbcc0d7242e99ce
Grayson-Remington <graysonbremington@gmail.com>
2022-11-14 16:24:51 -0500

    first commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: 'Terminal Website',
+    imgUrl: '',
+    stack: ['HTML', 'CSS', 'Javascript'],
+    link: 'https://fkcodes.com',
+  },
+  {
+    title: 'Developer Portfolio',
+    imgUrl: '',
+    stack: ['HTML', 'CSS', 'Javascript'],
+    link: 'https://fkcodes.com',
+  },
+  {
+    title: 'Terminal Website',
+    imgUrl: '',
+    stack: ['HTML', 'CSS', 'Javascript'],
+    link: 'https://fkcodes.com',
+  }
+]
