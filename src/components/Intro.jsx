6bb542af3c1c0800168310e13864992eb8962786
Grayson-Remington <graysonbremington@gmail.com>
2022-12-15 15:41:03 -0500

    -github pages test

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,27 +5,21 @@ function Intro() {
     <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
       <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>Gray</h1>
       <p className='text-base md: text-xl mb-3 font-medium'>Software Engineer & Web Developer</p>
-      <p className='text-sm max-w-xl mb-6 font-bold'> I'm an indie full-stack developer and content creator building my
-            version of the digital world one step at a time. All coding projects
-            are built from the ground up, from planning and designing all the way
-            to solving real-life problems with code.
-            <br />
-            All video content is built the same way, from ideation and planning,
-            all the way to finalizing the content with artistic touches. I publish
-            that content on my YouTube channel{' '}
-            <a
-               href="https://grayremington.com"
-               target="_blank"
-               className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600"
-               rel="noreferrer noopener"
-            >
-               GrayRemington
-            </a>{' '}
-            to more than 400k subscribers.
-         </p>
+      <p className='text-sm max-w-xl mb-6 font-bold'>
+          My journey started back in my highschool drafting class, where my passion for solving problems was
+             ignited by learning Autodesk software. I spent 3 years working with AutoCAD and Inventor before heading to 
+             Colorado State to study Mechanical Engineering. While at Colorado State, I worked at the university recreation center.
+              For 5 years, I honed my customer service and communication skills. After graduating, I joined a telecommunications
+               engineering company and was able to use both my communication and problem solving/design skills to build construction 
+               drawings for telecom companies. The only aspect of my job that I did not find desirable was the daily driving to 
+               sites all around Virginia. Web development is perfect for me as it allows me to continue using the problem solving 
+               and communication skills that I developed over the years, from a stable location.
+      </p>
+      </div>
+            
 
 
-    </div>
+    
   )
 }
 
