04f61253e77629d9fe39e2c347841923b68f2a30
Grayson-Remington <graysonbremington@gmail.com>
2022-12-15 15:44:48 -0500

    fixed links on index

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ function Intro() {
   return (
     <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
       <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>Gray</h1>
-      <p className='text-base md: text-xl mb-3 font-medium'>Software Engineer & Web Developer</p>
+      <p className='text-base md:text-xl mb-3 font-medium'>Software Engineer & Web Developer</p>
       <p className='text-sm max-w-xl mb-6 font-bold'>
           My journey started back in my highschool drafting class, where my passion for solving problems was
              ignited by learning Autodesk software. I spent 3 years working with AutoCAD and Inventor before heading to 
